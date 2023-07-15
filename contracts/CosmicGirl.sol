268c5ab2165df434c273cfdc601946fe9220845d
alireza alizadeh <alireza.aa9700@gmail.com>
2023-07-15 08:35:41 +0330

    another ERC721 contract

--- contracts/CosmicGirl.sol
+++ contracts/CosmicGirl.sol
@@ -1,22 +1,51 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.9;
 
-// imports
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol"; // for adding some token Ids
-import "@openzeppelin/contracts/utils/Strings.sol"; // converting int to str
-import "@openzeppelin/contracts/utils/Base64.sol"; // work with our SVGs
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract ChainBattles is ERC721URIStorage {
-    using Strings for uint256;
+contract Alireza is ERC721, ERC721Enumerable, ERC721URIStorage {
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
 
-    constructor() ERC721("Chain Battles", "CBTLS") {}
+    Counters.Counter private _tokenIdCounter;
+    uint256 MAX_SUPPLY = 10000;
 
-    function mint() public {
-        _tokenIds.increment(); // because the original number of count is zero
-        uint256 newItemId = _tokenIds.current();
-        _safeMint(msg.sender, newItemId);
+    constructor() ERC721("Alireza", "MHR") {}
+
+    function safeMint(address to, string memory uri) public {
+        uint256 tokenId = _tokenIdCounter.current();
+        require(tokenId <= MAX_SUPPLY, "all NFTs have been minted");
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 batchSize
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
