641ea850fddc32190a227d88226fda291d165e83
alireza alizadeh <alireza.aa9700@gmail.com>
2023-07-16 09:01:55 +0330

    add deploy section

--- test/unit/singleNftUnit.test.js
+++ test/unit/singleNftUnit.test.js
@@ -7,6 +7,19 @@ describe("Nft Marketplace Tests", () => {
     const PRICE = ethers.utils.parseEther("0.1")
     const TOKEN_ID = 0
     beforeEach(async () => {
+        const accounts = await ethers.getSigners()
+        // accounts
+        deployer = accounts[0]
+        player = accounts[1]
+
+        await deployments.fixture(["all"]) // deploy all of those contracts across deploy directory
+
+        // contracts
+
+        // nft --> ERC721 contract
+        const nftContract = await ethers.getContractFactory("CosmicGirl")
+        cosmigGirl = await nftContract.deploy()
+        await cosmigGirl.deployed()
         // other describes
         // cosmigGirl -> contract name
         // account name ---> deployer, user
