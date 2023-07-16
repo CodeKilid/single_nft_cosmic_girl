2b89fedecbabaeb2f2b32f775677e8df113b9180
alireza alizadeh <alireza.aa9700@gmail.com>
2023-07-16 08:56:49 +0330

    adding beforeEach

--- test/unit/singleNftUnit.test.js
+++ test/unit/singleNftUnit.test.js
@@ -3,8 +3,12 @@ const { ethers } = require("hardhat")
 
 describe("Nft Marketplace Tests", () => {
     // variables and contract deploying
-    let
-    // other describes
-    // consmigGirl -> contract name
-    // account name ---> deployer, user
+    let deployer, user, cosmigGirl
+    const PRICE = ethers.utils.parseEther("0.1")
+    const TOKEN_ID = 0
+    beforeEach(async () => {
+        // other describes
+        // cosmigGirl -> contract name
+        // account name ---> deployer, user
+    })
 })
