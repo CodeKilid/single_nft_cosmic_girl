ac540444e8a67301401803d3d133695e243d58dc
ElahiAli <ali_elahi00@outlook.com>
2023-07-16 09:01:18 +0330

    adding the describe of all functions

--- test/unit/singleNftUnit.test.js
+++ test/unit/singleNftUnit.test.js
@@ -1,27 +1,47 @@
-const { expect, assert } = require("chai")
-const { ethers } = require("hardhat")
+const { expect, assert } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("Nft Marketplace Tests", () => {
-    // variables and contract deploying
-    let deployer, user, cosmigGirl
-    const PRICE = ethers.utils.parseEther("0.1")
-    const TOKEN_ID = 0
-    beforeEach(async () => {
-        const accounts = await ethers.getSigners()
-        // accounts
-        deployer = accounts[0]
-        player = accounts[1]
+  // variables and contract deploying
+  let deployer, user, cosmigGirl;
+  const PRICE = ethers.utils.parseEther("0.1");
+  const TOKEN_ID = 0;
+  beforeEach(async () => {
+    const accounts = await ethers.getSigners();
+    // accounts
+    deployer = accounts[0];
+    player = accounts[1];
 
-        await deployments.fixture(["all"]) // deploy all of those contracts across deploy directory
+    await deployments.fixture(["all"]); // deploy all of those contracts across deploy directory
 
-        // contracts
+    // contracts
 
-        // nft --> ERC721 contract
-        const nftContract = await ethers.getContractFactory("CosmicGirl")
-        cosmigGirl = await nftContract.deploy()
-        await cosmigGirl.deployed()
-        // other describes
-        // cosmigGirl -> contract name
-        // account name ---> deployer, user
-    })
-})
+    // nft --> ERC721 contract
+    const nftContract = await ethers.getContractFactory("CosmicGirl");
+    cosmigGirl = await nftContract.deploy();
+    await cosmigGirl.deployed();
+    // other describes
+    // cosmigGirl -> contract name
+    // account name ---> deployer, user
+  });
+});
+
+describe("constructor", function () {
+  it("Intializing", async () => {});
+});
+
+describe("BeforeTokenTransfer", function () {
+  it("", async () => {});
+});
+
+describe("Burn", function () {
+  it("", async () => {});
+});
+
+describe("TokenURI", function () {
+  it("", async () => {});
+});
+
+describe("SupportsInterface", function () {
+  it("", async () => {});
+});
