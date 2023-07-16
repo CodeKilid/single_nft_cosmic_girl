029357e723d50a7863a046a8b027130a865b36c6
ElahiAli <ali_elahi00@outlook.com>
2023-07-16 08:24:47 +0330

    writing deploy script for deploying on testnet

---
+++ deploy/deploy.js
@@ -0,0 +1,30 @@
+const { network } = require("hardhat");
+const { developmentChains } = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
+
+module.exports = async function ({ deployments, getNamedAccounts }) {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  log(deployer);
+  const args = [];
+
+  const cosmicGirl = await deploy("CosmicGirl", {
+    from: deployer,
+    log: true,
+    args: args,
+  });
+
+  log(process.env.ETHERSCAN_API_KEY);
+
+  if (
+    !developmentChains.includes(network.name) &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    log("Verifying...");
+    await verify(cosmicGirl.address, args);
+  }
+  log("Deployed.");
+  log("-----------------------------------------------------------");
+};
+
+module.exports.tags = ["all", "CosmicGirl"];
