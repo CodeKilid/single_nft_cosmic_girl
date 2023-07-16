1e4a2fdd10c824681beb11a11b098da1b52dd990
ElahiAli <ali_elahi00@outlook.com>
2023-07-16 11:05:32 +0330

    change format

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -1,42 +1,39 @@
 const {
-  networkConfig,
-  developmentChains,
-  VERIFICATION_BLOCK_CONFIRMATIONS,
-} = require("../helper-hardhat-config.js");
-const { network } = require("hardhat");
-const { verify } = require("../utils/verify.js");
+    networkConfig,
+    developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+} = require("../helper-hardhat-config.js")
+const { network } = require("hardhat")
+const { verify } = require("../utils/verify.js")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
-  const { deploy, log } = deployments;
-  const { deployer } = await getNamedAccounts();
-  const waitBlockConfirmations = developmentChains.includes(network.name)
-    ? 1
-    : VERIFICATION_BLOCK_CONFIRMATIONS;
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const waitBlockConfirmations = developmentChains.includes(network.name)
+        ? 1
+        : VERIFICATION_BLOCK_CONFIRMATIONS
 
-  console.log(process.env.RPC_URL);
-  module.exports = async function ({ deployments, getNamedAccounts }) {
-    const { deploy, log } = deployments;
-    const { deployer } = await getNamedAccounts();
-    log(deployer);
-    const args = [];
+    console.log(process.env.RPC_URL)
+    module.exports = async function ({ deployments, getNamedAccounts }) {
+        const { deploy, log } = deployments
+        const { deployer } = await getNamedAccounts()
+        log(deployer)
+        const args = []
 
-    const arguments = [];
-    const cosmicGirl = await deploy("CosmicGirl", {
-      from: deployer,
-      args: arguments,
-      log: true,
-      waitConfirmations: waitBlockConfirmations,
-    });
-    if (
-      !developmentChains.includes(network.name) &&
-      process.env.ETHERSCAN_API_KEY
-    ) {
-      log("Verifying...");
-      await verify(cosmicGirl.address, arguments);
-      log("Verified!");
+        const arguments = []
+        const cosmicGirl = await deploy("CosmicGirl", {
+            from: deployer,
+            args: arguments,
+            log: true,
+            waitConfirmations: waitBlockConfirmations,
+        })
+        if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+            log("Verifying...")
+            await verify(cosmicGirl.address, arguments)
+            log("Verified!")
+        }
+        log("----------------------------------------------------")
     }
-    log("----------------------------------------------------");
-  };
 
-  module.exports.tags = ["all", "cosmicGirl"];
-};
+    module.exports.tags = ["all", "cosmicGirl"]
+}
