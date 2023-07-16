c66d91175d48ce66af0b3886b5152cbbc0499ae9
alireza alizadeh <alireza.aa9700@gmail.com>
2023-07-16 14:01:25 +0330

    delete the staging test

--- test/staging/singleNftStaging.test.js
+++
@@ -1,3 +0,0 @@
-const { assert, expect } = require("chai")
-const { deployments, ethers, getNamedAccounts, network } = require("hardhat")
-const { isCallTrace } = require("hardhat/internal/hardhat-network/stack-traces/message-trace")
