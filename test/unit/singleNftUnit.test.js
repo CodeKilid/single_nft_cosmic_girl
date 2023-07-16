1e16316214fe8cf58ea27f04a8030e9c6b379550
ElahiAli <ali_elahi00@outlook.com>
2023-07-16 10:16:52 +0330

    adding TokenURI test

--- test/unit/singleNftUnit.test.js
+++ test/unit/singleNftUnit.test.js
@@ -1,58 +1,62 @@
-const { expect, assert } = require("chai")
-const { ethers } = require("hardhat")
+const { expect, assert } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("Nft Marketplace Tests", () => {
-    // variables and contract deploying
-    let deployer, user, cosmicGirl
-    // const PRICE = ethers.utils.parseEther("0.1")
-    const TOKEN_ID = 0
-    beforeEach(async () => {
-        const accounts = await ethers.getSigners()
-        // accounts
-        deployer = accounts[0]
-        player = accounts[1]
+  // variables and contract deploying
+  let deployer, user, cosmicGirl;
+  // const PRICE = ethers.utils.parseEther("0.1")
+  const TOKEN_ID = 0;
+  beforeEach(async () => {
+    const accounts = await ethers.getSigners();
+    // accounts
+    deployer = accounts[0];
+    player = accounts[1];
 
-        // await deployments.fixture(["all"]) // deploy all of those contracts across deploy directory
+    // await deployments.fixture(["all"]) // deploy all of those contracts across deploy directory
 
-        // contracts
+    // contracts
 
-        // nft --> ERC721 contract
-        const nftContract = await ethers.getContractFactory("CosmicGirl")
-        cosmicGirl = await nftContract.deploy()
-        await cosmicGirl.safeMint(deployer.address)
-        // await cosmicGirl.deployed()
-        // other describes
-        // cosmicGirl -> contract name
-        // account name ---> deployer, user
-    })
+    // nft --> ERC721 contract
+    const nftContract = await ethers.getContractFactory("CosmicGirl");
+    cosmicGirl = await nftContract.deploy();
+    await cosmicGirl.safeMint(deployer.address);
+    // await cosmicGirl.deployed()
+    // other describes
+    // cosmicGirl -> contract name
+    // account name ---> deployer, user
+  });
 
-    describe("constructor", function () {
-        it("Intializing", async () => {})
-    })
+  describe("constructor", function () {
+    it("Intializing", async () => {});
+  });
 
-    describe("SafeMint", function () {
-        it("want sure minting works", async () => {
-            expect(await cosmicGirl.safeMint(deployer.address))
-        })
-        it("sure the owner of nft after changing", async () => {
-            cosmicGirl.safeTransferFrom()
-        })
+  describe("SafeMint", function () {
+    it("want sure minting works", async () => {
+      expect(await cosmicGirl.safeMint(deployer.address));
+    });
+    it("sure the owner of nft after changing", async () => {
+      cosmicGirl.safeTransferFrom();
+    });
+  });
 
-    })
+  describe("BeforeTokenTransfer", function () {
+    it("", async () => {});
+  });
 
-    describe("BeforeTokenTransfer", function () {
-        it("", async () => {})
-    })
+  describe("Burn", function () {
+    it("", async () => {});
+  });
 
-    describe("Burn", function () {
-        it("", async () => {})
-    })
+  describe("TokenURI", function () {
+    it("getting token uri", async () => {
+      const expectedTokenUri =
+        "https://ipfs.io/ipfs/Qma2rdJ7JxmSbn3tydFrU27Er9eJPeTYNHk19mFiF9FeDv";
+      const tokenUri = await cosmicGirl.getTokenUri();
+      assert.equal(expectedTokenUri, tokenUri);
+    });
+  });
 
-    describe("TokenURI", function () {
-        it("", async () => {})
-    })
-
-    describe("SupportsInterface", function () {
-        it("", async () => {})
-    })
-})
+  describe("SupportsInterface", function () {
+    it("", async () => {});
+  });
+});
