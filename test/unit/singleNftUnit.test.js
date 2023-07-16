31c1cd94ed449a8e9dd54ecdc324ec386aa1a431
alireza alizadeh <alireza.aa9700@gmail.com>
2023-07-16 08:51:31 +0330

    template test

--- test/unit/singleNftUnit.test.js
+++ test/unit/singleNftUnit.test.js
@@ -1,2 +1,7 @@
 const { expect, assert } = require("chai")
 const { ethers } = require("hardhat")
+
+describe("Nft Marketplace Tests", () => {
+    // variables and contract deploying
+    // other describes
+})
