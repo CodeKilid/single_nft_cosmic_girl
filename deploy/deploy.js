bb647e57734323f32b18f26fa0bc11c3ec2bdbd1
alireza alizadeh <alireza.aa9700@gmail.com>
2023-07-16 08:34:01 +0330

    git add .git add .

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -1,7 +1,13 @@
 const { network } = require("hardhat");
 const { developmentChains } = require("../helper-hardhat-config");
 const { verify } = require("../utils/verify");
+<<<<<<< HEAD
 
+=======
+require("dotenv").config();
+
+console.log(process.env.RPC_URL);
+>>>>>>> 598a620 (delpoy added)
 module.exports = async function ({ deployments, getNamedAccounts }) {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
@@ -13,9 +19,13 @@ module.exports = async function ({ deployments, getNamedAccounts }) {
     log: true,
     args: args,
   });
+<<<<<<< HEAD
 
   log(process.env.ETHERSCAN_API_KEY);
 
+=======
+  log(process.env.ETHERSCAN_API_KEY);
+>>>>>>> 598a620 (delpoy added)
   if (
     !developmentChains.includes(network.name) &&
     process.env.ETHERSCAN_API_KEY
@@ -28,3 +38,9 @@ module.exports = async function ({ deployments, getNamedAccounts }) {
 };
 
 module.exports.tags = ["all", "CosmicGirl"];
+<<<<<<< HEAD
+=======
+
+// mumbai testnet contract address = 0x1aaFdaD0df01546935aabAf64F5E9738281E452E
+// sepolia testnet contract address = 0x719f567537E7d274b40a8Eb0Af7F3ADe632F7C1a
+>>>>>>> 598a620 (delpoy added)
