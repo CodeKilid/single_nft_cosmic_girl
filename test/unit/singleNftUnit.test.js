7e29f6351939fb73ce57767c075add2d1c2d44af
ElahiAli <ali_elahi00@outlook.com>
2023-07-17 14:27:54 +0330

    change test file

--- test/unit/singleNftUnit.test.js
+++ test/unit/singleNftUnit.test.js
@@ -1,9 +1,9 @@
 const { expect, assert } = require("chai")
 const { ethers } = require("hardhat")
 
-describe("Nft Marketplace Tests", () => {
+describe("Cosmic Girl V2 Tests", () => {
     // variables and contract deploying
-    let deployer, player, cosmicGirl
+    let deployer, player, cosmicGirlV2
     // const PRICE = ethers.utils.parseEther("0.1")
     const TOKEN_ID = 0
     beforeEach(async () => {
@@ -17,62 +17,95 @@ describe("Nft Marketplace Tests", () => {
         // contracts
 
         // nft --> ERC721 contract
-        const nftContract = await ethers.getContractFactory("CosmicGirl")
-        cosmicGirl = await nftContract.deploy()
-        // await cosmicGirl.safeMint(deployer.address)
+        const nftContract = await ethers.getContractFactory("CosmicGirlV2")
+        cosmicGirlV2 = await nftContract.deploy()
+        // await cosmicGirlV2.safeMint(deployer.address)
 
         // other describes
-        // cosmicGirl -> contract name
+        // cosmicGirlV2 -> contract name
         // account name ---> deployer, user
     })
 
     describe("constructor", function () {
         it("Intializing", async () => {
-            const name = await cosmicGirl.name()
-            const symbol = await cosmicGirl.symbol()
-            // tokenId = 0
+            const name = await cosmicGirlV2.name()
+            const symbol = await cosmicGirlV2.symbol()
+            const initialTokenId = await cosmicGirlV2.getTokenId()
+            const expectedTokenId = 0
 
             assert.equal(name, "CosmicGirl")
             assert.equal(symbol, "CS")
+            assert.equal(initialTokenId, expectedTokenId)
         })
     })
 
     describe("SafeMint", function () {
         it("want sure minting works", async () => {
-            expect(await cosmicGirl.safeMint(deployer.address))
+            expect(await cosmicGirlV2.safeMint(deployer.address))
         })
-        it("sure the owner of nft after changing", async () => {
+        it("sure the owner of nft after changing V2", async () => {
             let tokenId
 
-            await cosmicGirl.safeMint(deployer.address)
-            tokenId = (Number(await cosmicGirl.getTokenId()) - 1)
+            await cosmicGirlV2.safeMint(deployer.address)
+            tokenId = Number(await cosmicGirlV2.getTokenId()) - 1
             console.log(`first : ${tokenId}`)
-            const currentOwner = await cosmicGirl.ownerOf(tokenId)
-            
+            const currentOwner = await cosmicGirlV2.ownerOf(tokenId)
+
             // expect deployer that minted be the owner
             expect(currentOwner).to.equal(deployer.address)
             console.log(`current owner: ${currentOwner}`)
 
             // Change ownership of the NFT from the deployer to the player
-            await cosmicGirl.safeTransferFrom(deployer.address, player.address, tokenId)
-            await cosmicGirl.connect(player).safeMint(player.address)
-
-            tokenId = (Number(await cosmicGirl.getTokenId()) - 1)
-            const afterOwner = await cosmicGirl.ownerOf(tokenId)
+            await cosmicGirlV2.safeTransferFrom(currentOwner, player.address, tokenId)
+            tokenId = Number(await cosmicGirlV2.getTokenId()) - 1
+            const afterOwner = await cosmicGirlV2.ownerOf(tokenId)
             console.log(`second : ${tokenId}`)
+            console.log(`second owner: ${await cosmicGirlV2.ownerOf(tokenId)}`)
             expect(afterOwner).to.equal(player.address)
+        })
+
+        it("only owner can mint the nft", async () => {
+            await expect(cosmicGirlV2.connect(player).safeMint(player.address)).to.be.revertedWith(
+                "Ownable: caller is not the owner",
+            )
+        })
+
+        it("it can be minted only once", async () => {
+            await cosmicGirlV2.safeMint(deployer.address)
+            const tokenId = Number(await cosmicGirlV2.getTokenId()) - 1
+            await cosmicGirlV2.safeTransferFrom(deployer.address, player.address, tokenId)
+            await expect(cosmicGirlV2.connect(player).safeMint(player.address)).to.be.revertedWith(
+                "Ownable: caller is not the owner",
+            )
         })
     })
 
-  
     describe("TokenURI", function () {
-        it("getting token uri", async () => {
+        it("getting token uri V2", async () => {
+            await cosmicGirlV2.safeMint(deployer.address)
+            const tokenId = Number(await cosmicGirlV2.getTokenId()) - 1
             const expectedTokenUri =
                 "https://ipfs.io/ipfs/Qma2rdJ7JxmSbn3tydFrU27Er9eJPeTYNHk19mFiF9FeDv"
-            const tokenUri = await cosmicGirl.getTokenUri()
+            const tokenUri = await cosmicGirlV2.tokenURI(tokenId)
+
             assert.equal(expectedTokenUri, tokenUri)
         })
     })
 
+    describe("Burn Nft", function () {
+        it("only owner can burn the nft", async () => {
+            await cosmicGirlV2.safeMint(deployer.address)
+            const tokenId = Number(await cosmicGirlV2.getTokenId()) - 1
+            expect(await cosmicGirlV2.connect(player).burnToken(tokenId)).to.be.revertedWith(
+                "Ownable: caller is not the owner",
+            )
+        })
+        it("should burn the nft", async () => {
+            await cosmicGirlV2.safeMint(deployer.address)
+            const tokenId = Number(await cosmicGirlV2.getTokenId()) - 1
+            const contractAddress = await cosmicGirlV2.burnToken(tokenId)
 
+            console.log(contractAddress)
+        })
+    })
 })
